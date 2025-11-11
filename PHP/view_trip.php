59bac08f4a96ca49df3227fe24896887bb78ef8d
jstinapay <lmiguenjusti@gmail.com>
2025-11-11 23:58:07 +0800

    Adjust trip card width and remove view button
    Reduced the width of .trip_card in dashboard.css from 50% to 35% for improved layout. Removed the 'View' button from category_actions in view_trip.php, leaving only 'Edit' and 'Delete' options.

--- PHP/view_trip.php
+++ PHP/view_trip.php
@@ -126,6 +126,7 @@ $conn->close();
                 <span>Create Trip</span>
             </a>
         </li>
+        </li>
         <li>
             <a href="profile.php">
                 <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M234-276q51-39 114-61.5T480-360q69 0 132 22.5T726-276q35-41 54.5-93T800-480q0-133-93.5-226.5T480-800q-133 0-226.5 93.5T160-480q0 59 19.5 111t54.5 93Zm246-164q-59 0-99.5-40.5T340-580q0-59 40.5-99.5T480-720q59 0 99.5 40.5T620-580q0 59-40.5 99.5T480-440Zm0 360q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q53 0 100-15.5t86-44.5q-39-29-86-44.5T480-280q-53 0-100 15.5T294-220q39 29 86 44.5T480-160Zm0-360q26 0 43-17t17-43q0-26-17-43t-43-17q-26 0-43 17t-17 43q0 26 17 43t43 17Zm0-60Zm0 360Z"/></svg>
@@ -195,7 +196,6 @@ $conn->close();
                 <p>Budget: <?php echo $symbol; ?><?php echo number_format($budget['allocated_budget'], 2); ?></p>
 
                 <div class="category_actions">
-                    <a href="view_category_budget.php?id=<?php echo $budget['category_budget_id']; ?>" class="view-button">View</a>
                     <a href="edit_category_budget.php?id=<?php echo $budget['category_budget_id']; ?>" class="edit-button">Edit</a>
                     <a href="delete_category_budget.php?id=<?php echo $budget['category_budget_id']; ?>&trip_id=<?php echo $trip['trip_id']; ?>" class="delete-button" onclick="return confirm('Are you sure you want to delete this budget? This action cannot be undone.');">Delete</a>
                 </div>
